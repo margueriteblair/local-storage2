5dc5ebb65a479f63357a60ca484a36477ac06f27
margueriteblair <62972798+margueriteblair@users.noreply.github.com>
2020-05-10 23:23:49 -0400

    Add files via upload

--- practicaljs2.js
+++ practicaljs2.js
@@ -1,26 +1,110 @@
 //place to store todos
 //programming term for a list is an 'array'
 
-let todos = ["task1", "task2", "task3"];
 
-function displayTodos() {
-    console.log("My Todos:", todos);
-}
-displayTodos();
+let todoList = {
+    todos: [],
+     displayTodos: function() {
+        if (this.todos.length === 0) {
+            console.log(`Your todo list is empty!`)
+        } else {
+            console.log('My todos:');
+            for (let i = 0; i < this.todos.length; i++) {
+               if (this.todos[i].completed === true) {
+                   console.log(`(x)`, this.todos[i].todoText);
+               } else {
+                   console.log(`()`, this.todos[i].todoText);
+               }
+        }
+        }
+     },
 
-//way to add new todos
-function addTodos(todo) {
-    todos.push(todo);
-    displayTodos();
-}
-addTodos();
+     addTodos: function(todoText) {
+         this.todos.push({
+             todoText: todoText,
+             completed: false
+        });
+         this.displayTodos()
+     },
 
-function changeTodo(position, newValue) {
-    todos[position] = newValue;
-    displayTodos()
+     changeTodo: function(position, todoText) {
+        this.todos[position].todoText = todoText;
+        this.displayTodos();
+     },
+
+     deleteTodo: function(position) {
+         this.todos.splice(position, 1);
+         this.displayTodos();
+     },
+
+     toggleCompleted: function(position) {
+        let todo = this.todos[position];
+        todo.completed = !todo.completed;
+        this.displayTodos();
+     },
+     toggleAll: function() {
+        let totalTodos = this.todos.length;
+        let completedTodos = 0;
+
+        for (let i = 0; i < totalTodos; i++) {
+            if (this.todos[i].completed === true) {
+                completedTodos++;
+            }
+        }
+        if(completedTodos === totalTodos) {
+            for(let i = 0; i < totalTodos; i++) {
+                this.todos[i].completed = false;
+            }
+        } else {
+            for (let i = 0; i < totalTodos; i++) {
+                this.todos[i].completed = true;   
+            }
+        }
+        this.displayTodos();
+     }
+};
+
+// let displayTodosButton = document.getElementById("displayTodosButton");
+// let toggleAllButton = document.getElementById("toggleAllButton")
+
+// displayTodosButton.addEventListener("click", function() {
+//     todoList.displayTodos();
+
+// })
+
+// toggleAllButton.addEventListener("click", function(){
+//     todoList.toggleAll();
+// })
+
+let handlers = {
+    displayTodos: function() {
+        todoList.displayTodos();
+    },
+    toggleAll: function() {
+        todoList.toggleAll();
+    },
+    addTodos: function() {
+        let addTodoTextInput = document.getElementById("addTodoTextInput");
+        todoList.addTodos(addTodoTextInput.value);
+        addTodoTextInput.value = "";
+    },
+    changeTodo: function() {
+        let changeTodoPositionInput = document.getElementById("changeTodoPositionInput")
+        let changeTodoTextInput = document.getElementById("changeTodoTextInput")
+        todoList.changeTodo(changeTodoPositionInput.valueAsNumber, changeTodoTextInput.value);
+        changeTodoPositionInput.value = "";
+        changeTodoTextInput.value = "";
+    },
+    deleteTodo: function() {
+        let deleteTodoPositionInput = document.getElementById("deleteTodoPositionInput");
+        todoList.deleteTodo(deleteTodoPositionInput.valueAsNumber);
+        deleteTodoPositionInput.value = "";
+    },
+    toggleCompleted: function() {
+        let toggleCompletedPositionInput = document.getElementById("toggleCompletedPositionInput")
+        todoList.toggleCompleted(toggleCompletedPositionInput.valueAsNumber);
+        toggleCompletedPositionInput.value ="";
+    }
 }
 
-function deleteTodo(position) {
-    todos.splice(position, 1)
-    displayTodos();
-}
+
