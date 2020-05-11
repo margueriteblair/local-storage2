43c9165bd8fd3a7baab4a549a5cfecc0512c3c52
margueriteblair <62972798+margueriteblair@users.noreply.github.com>
2020-05-10 20:59:51 -0400

    Add files via upload

--- practicaljs2.js
+++ practicaljs2.js
@@ -1,26 +1,47 @@
 //place to store todos
 //programming term for a list is an 'array'
 
-let todos = ["task1", "task2", "task3"];
 
-function displayTodos() {
-    console.log("My Todos:", todos);
-}
-displayTodos();
+let todoList = {
+    todos: [],
+     displayTodos: function() {
+        if (this.todos.length === 0) {
+            console.log(`Your todo list is empty!`)
+        } else {
+            console.log('My todos:');
+            for (let i = 0; i < this.todos.length; i++) {
+               if (this.todos[i].completed === true) {
+                   console.log(`(x)`, this.todos[i].todoText);
+               } else {
+                   console.log(`()`, this.todos[i].todoText);
+               }
+        }
+        }
+     },
 
-//way to add new todos
-function addTodos(todo) {
-    todos.push(todo);
-    displayTodos();
-}
-addTodos();
+     addTodos: function(todoText) {
+         this.todos.push({
+             todoText: todoText,
+             completed: false
+        });
+         this.displayTodos()
+     },
 
-function changeTodo(position, newValue) {
-    todos[position] = newValue;
-    displayTodos()
-}
+     changeTodo: function(position, todoText) {
+        this.todos[position].todoText = todoText;
+        this.displayTodos();
+     },
 
-function deleteTodo(position) {
-    todos.splice(position, 1)
-    displayTodos();
-}
+     deleteTodo: function(position) {
+         this.todos.splice(position, 1);
+         this.displayTodos();
+     },
+
+     toggleCompleted: function(position) {
+        let todo = this.todos[position];
+        todo.completed = !todo.completed;
+        this.displayTodos();
+     }
+};
+
+
