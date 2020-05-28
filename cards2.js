3a76b962df472fbca0e4ffc53aa158298a0d3827
margueriteblair <62972798+margueriteblair@users.noreply.github.com>
2020-05-28 15:42:48 -0400

    Add files via upload
    laid the foundation to make a blackjack game with a full deck of card images

---
+++ cards2.js
@@ -0,0 +1,43 @@
+let hit = document.createElement("button")
+hit.id = "hit"
+hit.textContent = "Hit Me"
+document.body.appendChild(hit)
+hit.onclick = function() {
+    
+}
+
+const cardDeck = [
+    {
+        value: 2,
+        img: "images/2C.png"
+    },
+    {
+        value: 2,
+        img: "images/2D.png"
+    },
+    {
+        value: 2,
+        img: "images/2H.png"
+    },
+    {
+        value: 2,
+        img: "images/2S.png"
+    },
+    {
+        value: 3,
+        img: "images/3C.png"
+    },
+    {
+        value: 3,
+        img: "images/3D.png"
+    },
+    {
+        value: 3,
+        img: "images/2H.png"
+    },
+    {
+        value: 3,
+        img: "images/2S.png"
+    },
+]
+
